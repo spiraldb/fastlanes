64a66674eb5e0a306b7244e257c8b41e217604b1
Joe Isaacs <joe.isaacs@live.co.uk>
2025-03-03 15:47:05 +0000

    fix

--- benches/bitpacking_cmp.rs
+++ benches/bitpacking_cmp.rs
@@ -11,10 +11,11 @@ mod bench {
     use divan::Bencher;
     use fastlanes::{BitPacking, BitPackingCompare, FastLanesComparable};
     use num_traits::FromPrimitive;
+    use std::hint::black_box;
 
-    const BENCH_W: [usize; 4] = [2, 3, 5, 7];
+    const BENCH_W: [usize; 6] = [2, 3, 5, 7, 15, 31];
 
-    #[divan::bench(types=[u16, u32, u64], consts = BENCH_W)]
+    #[divan::bench(types=[u32, u64], consts = BENCH_W)]
     fn bitpacking_cmp_fused<T, const W: usize>(bencher: Bencher)
     where
         T: BitPacking + FastLanesComparable<Bitpacked = T> + FromPrimitive + Copy,
@@ -36,13 +37,14 @@ mod bench {
                     &packed,
                     &mut unpacked,
                     |a, b| a == b,
-                    value,
-                )
+                    black_box(value),
+                );
+                black_box(unpacked)
             };
         });
     }
 
-    #[divan::bench(types=[u16, u32, u64], consts = BENCH_W)]
+    #[divan::bench(types=[u32, u64], consts = BENCH_W)]
     fn bitpacking_cmp_seq<T, const W: usize>(bencher: Bencher)
     where
         T: BitPacking + FromPrimitive + Copy,
@@ -55,14 +57,16 @@ mod bench {
         unsafe { T::unchecked_pack(W, &values, &mut packed) };
 
         let mut unpacked = [T::zero(); 1024];
+        let mut bools = [0u64; 16];
 
         bencher.bench_local(|| {
             unsafe { T::unchecked_unpack(W, &packed, &mut unpacked) };
-            collect_bool_cmp(&unpacked, &value)
+            collect_bool_cmp(&unpacked, &black_box(value), black_box(&mut bools));
+            black_box(bools)
         });
     }
 
-    #[divan::bench(types=[u16, u32, u64], consts = BENCH_W)]
+    #[divan::bench(types=[u32, u64], consts = BENCH_W)]
     fn bitpacking_cmp_unpack<T, const W: usize>(bencher: Bencher)
     where
         T: BitPacking + FromPrimitive + Copy,
@@ -80,17 +84,17 @@ mod bench {
         });
     }
 
-    pub fn collect_bool_cmp<T: PartialEq + Copy>(unpacked: &[T; 1024], cmp: &T) -> Vec<u64> {
-        collect_bool(unpacked.len(), |idx| unpacked[idx] == *cmp)
+    pub fn collect_bool_cmp<T: PartialEq + Copy>(
+        unpacked: &[T; 1024],
+        cmp: &T,
+        output: &mut [u64; 16],
+    ) {
+        collect_bool(|idx| unpacked[idx] == *cmp, output)
     }
 
     #[inline]
-    pub fn collect_bool<F: FnMut(usize) -> bool>(len: usize, mut f: F) -> Vec<u64> {
-        let mut buffer = Vec::with_capacity(len.div_ceil(4) * 8);
-
-        let chunks = len / 64;
-        let remainder = len % 64;
-        for chunk in 0..chunks {
+    pub fn collect_bool<F: FnMut(usize) -> bool>(mut f: F, output: &mut [u64; 16]) {
+        for chunk in 0..16 {
             let mut packed = 0;
             for bit_idx in 0..64 {
                 let i = bit_idx + chunk * 64;
@@ -98,21 +102,7 @@ mod bench {
             }
 
             // SAFETY: Already allocated sufficient capacity
-            buffer.push(packed);
+            output[chunk] = packed;
         }
-
-        if remainder != 0 {
-            let mut packed = 0;
-            for bit_idx in 0..remainder {
-                let i = bit_idx + chunks * 64;
-                packed |= u64::from(f(i)) << bit_idx;
-            }
-
-            // SAFETY: Already allocated sufficient capacity
-            buffer.push(packed);
-        }
-
-        buffer.truncate(len.div_ceil(8));
-        buffer
     }
 }
