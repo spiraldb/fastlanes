c5203b2bae53f1e774893a3fc6b53ca28d493bba
Adam Gutglick <adam@spiraldb.com>
2026-07-27 12:56:27 +0100

    Refactor the proptest
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

--- tests/proptest.rs
+++ tests/proptest.rs
@@ -5,6 +5,7 @@ use fastlanes::BitPacking;
 use hegel::TestCase;
 use hegel::generators as gs;
 use hegel::generators::Integer;
+use pastey::paste;
 use std::fmt::Debug;
 
 const BUFFER_SIZE: usize = 1024;
@@ -15,19 +16,10 @@ where
 {
     let width = tc.draw(gs::integers::<usize>().min_value(0).max_value(T::T));
 
-    let max_value = if width == T::T {
-        T::max_value()
-    } else {
-        (T::one() << width) - T::one()
-    };
     let input = tc.draw(
-        gs::vecs(
-            gs::integers::<T>()
-                .min_value(T::zero())
-                .max_value(max_value),
-        )
-        .min_size(BUFFER_SIZE)
-        .max_size(BUFFER_SIZE),
+        gs::vecs(gs::integers::<T>())
+            .min_size(BUFFER_SIZE)
+            .max_size(BUFFER_SIZE),
     );
 
     // We use dirty buffers here, to make sure its not a hidden invariant
@@ -49,13 +41,6 @@ where
     let expected = input.iter().copied().map(|v| v & mask).collect::<Vec<_>>();
 
     assert_eq!(expected, unpacked_output);
-
-    for index in 0..BUFFER_SIZE {
-        assert_eq!(
-            unsafe { T::unchecked_unpack_single(width, &packed_output, index) },
-            expected[index],
-        );
-    }
 }
 
 fn assert_bitpack_repack_roundtrip<T>(tc: &TestCase)
@@ -80,37 +65,47 @@ where
     assert_eq!(packed_input, repacked_output);
 }
 
-macro_rules! bitpack_property_test {
-    ($name:ident, $type:ty, $property:ident) => {
-        #[hegel::test(test_cases = 1000)]
-        fn $name(tc: TestCase) {
-            $property::<$type>(&tc);
+fn assert_bitpack_unpack_single_matches_bulk<T>(tc: &TestCase)
+where
+    T: BitPacking + Debug + Integer + 'static,
+{
+    let width = tc.draw(gs::integers::<usize>().min_value(0).max_value(T::T));
+    let packed_length = (BUFFER_SIZE * width) / T::T;
+    let packed_input = tc.draw(
+        gs::vecs(gs::integers::<T>())
+            .min_size(packed_length)
+            .max_size(packed_length),
+    );
+    let index = tc.draw(
+        gs::integers::<usize>()
+            .min_value(0)
+            .max_value(BUFFER_SIZE - 1),
+    );
+
+    let mut unpacked_output = vec![T::one(); BUFFER_SIZE];
+    unsafe {
+        T::unchecked_unpack(width, &packed_input, &mut unpacked_output);
+    }
+
+    assert_eq!(
+        unsafe { T::unchecked_unpack_single(width, &packed_input, index) },
+        unpacked_output[index],
+    );
+}
+
+macro_rules! bitpack_property_tests {
+    ($property:ident for $($type:ident),+ $(,)?) => {
+        paste! {
+            $(
+                #[hegel::test(test_cases = 1000)]
+                fn [<test_ $property _ $type>](tc: TestCase) {
+                    [<assert_ $property>]::<$type>(&tc);
+                }
+            )+
         }
     };
 }
 
-bitpack_property_test!(test_bitpack_roundtrip_u8, u8, assert_bitpack_roundtrip);
-bitpack_property_test!(test_bitpack_roundtrip_u16, u16, assert_bitpack_roundtrip);
-bitpack_property_test!(test_bitpack_roundtrip_u32, u32, assert_bitpack_roundtrip);
-bitpack_property_test!(test_bitpack_roundtrip_u64, u64, assert_bitpack_roundtrip);
-
-bitpack_property_test!(
-    test_bitpack_repack_roundtrip_u8,
-    u8,
-    assert_bitpack_repack_roundtrip
-);
-bitpack_property_test!(
-    test_bitpack_repack_roundtrip_u16,
-    u16,
-    assert_bitpack_repack_roundtrip
-);
-bitpack_property_test!(
-    test_bitpack_repack_roundtrip_u32,
-    u32,
-    assert_bitpack_repack_roundtrip
-);
-bitpack_property_test!(
-    test_bitpack_repack_roundtrip_u64,
-    u64,
-    assert_bitpack_repack_roundtrip
-);
+bitpack_property_tests!(bitpack_roundtrip for u8, u16, u32, u64);
+bitpack_property_tests!(bitpack_repack_roundtrip for u8, u16, u32, u64);
+bitpack_property_tests!(bitpack_unpack_single_matches_bulk for u8, u16, u32, u64);
